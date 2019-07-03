7d4c472b9e811c6c92099b5055f2a648f51ead71
travis rodowick <travrodowick@yahoo.com>
2019-07-03 14:37:48 -0700

    added some div's and id's to HTML and declaring variables in .js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+console.log('ready!');
+// declare variables
+var currentGuess = 0;
+var targetNum = 0;
+var wins = 0;
+var loses = 0;
+var directions = ['play this game if you aint lame'];
+
+
+
+//closing $(document).ready(function(){})    
+})
