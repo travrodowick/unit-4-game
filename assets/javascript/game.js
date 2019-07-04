9c8d15920d54aa52b9a90f78f9abd375f4d5d179
travis rodowick <travrodowick@yahoo.com>
2019-07-03 17:43:22 -0700

    verified operational targetNum generation --- still returning 0 for each crystal value

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,7 @@ var wins = 0;
 var loses = 0;
 var directions = ['play this game if you aint lame'];
 // crystal values randomly generated between 1 - 12
+var crystalValue = [];
 var crystals = {
     redCrystal: crystalValue,
     blueCrystal: crystalValue,
@@ -19,19 +20,21 @@ var crystals = {
 //define functions
     //generate target number between 
     function getTargetNum() {
-        return Math.floor(Math.random() * 102) + 19;
-      }
+        targetNum = Math.floor(Math.random() * 102) + 19;
+        console.log(targetNum)
+    }
 
     //generate 1 - 12 values for crystals
     function crystalGenerator() {
         for (var i = 0; i < crystals.length; i++ ){
-            var crystalValue = Math.floor(Math.random() * 12) + 1;
+            var crystalValue = [Math.floor(Math.random() * 12) + 1];
         }
         console.log (crystals)
     };
 
 
-
+getTargetNum()
+crystalGenerator()
 
 //closing $(document).ready(function(){})    
 })
