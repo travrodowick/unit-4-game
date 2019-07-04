aee5c74d47309f6f8804a57c6749a517fb4e7773
travis rodowick <travrodowick@yahoo.com>
2019-07-03 17:34:11 -0700

    updates to HTML links and MORE IMPORTANTLY woorking through crystal value generation logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,26 +3,33 @@ console.log('ready!');
 
 // declare variables
 var currentGuess = 0;
-// target randomly generated between 19 - 120
+// target = randomly generated between 19 - 120
 var targetNum = 0;
 var wins = 0;
 var loses = 0;
 var directions = ['play this game if you aint lame'];
 // crystal values randomly generated between 1 - 12
-var redCrystal = 0
-var blueCrystal = 0
-var yellowCrystal = 0
-var greenCrystal = 0
+var crystals = {
+    redCrystal: crystalValue,
+    blueCrystal: crystalValue,
+    yellowCrystal: crystalValue,
+    greenCrystal: crystalValue
+};
 
 //define functions
-    //generate target number
-    function getTargetNum(min, max) {
-        min = Math.ceil(19);
-        max = Math.floor(120);
-        return Math.floor(Math.random() * (max - min + 1)) + min; 
+    //generate target number between 
+    function getTargetNum() {
+        return Math.floor(Math.random() * 102) + 19;
       }
 
-    
+    //generate 1 - 12 values for crystals
+    function crystalGenerator() {
+        for (var i = 0; i < crystals.length; i++ ){
+            var crystalValue = Math.floor(Math.random() * 12) + 1;
+        }
+        console.log (crystals)
+    };
+
 
 
 
