e9659150c19e3c0797a9eb4ac534d13828f9de77
travis rodowick <travrodowick@yahoo.com>
2019-07-09 23:27:10 -0700

    fixed issues with crystal values -- changed function structure -- added to css reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,103 +8,95 @@ $(document).ready(function(){
     var wins = 0;
     var loses = 0;
     var directions = ['play this game if you aint lame'];
-    var crystals;
+    var value;
 //FUNCTION
 //=========================================================================
     //generate target number between 
     function getTargetNum() {
         targetNum = Math.floor(Math.random() * 102) + 19;
-        console.log (targetNum);
+        console.log ('target', targetNum);
     }
 
     //generate 1 - 12 values for crystals
+      function crystalValueGen () {
+        value = Math.floor(Math.random() * 12) + 1;
+        return value;
+      }
+
       function crystalGenerator() {
-            return {
-                crystals = {
-                    red: {
-                        points:  Math.floor(Math.random() * 12) + 1,
-                        immageURL:  
-                    },
-                    blue: {
-                        points:  Math.floor(Math.random() * 12) + 1,
-                        immageURL:
-                    },
-                    yellow: {
-                        points:  Math.floor(Math.random() * 12) + 1,
-                        immageURL:
-                    },
-                    green: {
-                        points:  Math.floor(Math.random() * 12) + 1,
-                        immageURL:
-                    },
-                }
-            }
+                    red = crystalValueGen()
+                    console.log('red', red)
+                    blue = crystalValueGen()
+                    console.log ('blue', blue)
+                    yellow = crystalValueGen()
+                    console.log('yellow', yellow)
+                    green = crystalValueGen()
+                    console.log('green', green)
         };
 
     //reset game
     function gameReset () {
+        currentScore = 0
+        updatedScore = 0
         $('#crystal-count').text('Current Score:  ' + currentScore);
         crystalGenerator();
-       //$('#crystal-container')//notsureyet
         getTargetNum();
-        $('#targetNumber').text(targetNum);
+        $('#targetNumber').text('Target Number:  ' + targetNum);
     };
 
     //update game stats
     function winnerLoser () {
-        if (userScore === targetNum) {
+        if (updatedScore === targetNum) {
             alert('Winner Winner Chicken Dinner!!!');
             gameReset();
             wins++;
             $('#winCounter').text('Wins:  ' + wins);
         }
-        if (userScore > targetNum) {
+        if (updatedScore > targetNum) {
             alert ('Bust!! How does it feel to lose, punk?');
             gameReset();
             loses++;
-            $('lossCounter').text('Losses:  ' + loses);
+            $('#lossCounter').text('Losses:  ' + loses);
         }
     }
 
     //update current score
-    function updateScore () {
-        var updatedScore = [currentScore += updatedScore]
-        $('crystal-count').text('Current Score:  ' + updatedScore)
-    };
+    var updatedScore = 0;
 
 
 
     //Testing / Debugging
     console.log('ready!');
-    getTargetNum();
-    //crystalGenerator();
-    console.log (crystals)
     console.log (currentScore);
 
 
 //MAIN PROCESS
 //=====================================================================
+    //start
+    gameReset()
 
     //register crystal clicks
-    $('#redCrystalTag').on('click', function(event) {
-        console.log (crystal)
-        updateScore()
-        winnerLoser()
+    $('#redCrystalTag').on('click', function() {
+        updatedScore += red;
+        $('#crystal-count').text('Current Score:  ' + updatedScore);
+        winnerLoser();
     }); 
-    $('#blueCrystalTag').on('click', function(event) {
-        console.log (crystal)
-        updateScore()
-        winnerLoser()
+    $('#blueCrystalTag').on('click', function() {
+        updatedScore += blue;
+        $('#crystal-count').text('Current Score:  ' + updatedScore);
+        winnerLoser();
     }); 
-    $('#greenCrystalTag').on('click', function(event) {
-        console.log (crystal)
-        updateScore()
-        winnerLoser()
+    $('#greenCrystalTag').on('click', function() {
+        updatedScore += green;
+        $('#crystal-count').text('Current Score:  ' + updatedScore);
+        winnerLoser();
     }); 
-    $('#yellowCrystalTag').on('click', function(event) {
-        console.log (crystal)
-        updateScore()
-        winnerLoser()
+    $('#yellowCrystalTag').on('click', function() {
+        updatedScore += yellow;
+        $('#crystal-count').text('Current Score:  ' + updatedScore);
+        winnerLoser();
     }); 
+
+
 //closing $(document).ready(function(){})    
 })
